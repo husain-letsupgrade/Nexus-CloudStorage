33cdcc8be78008484d5b5a69615098e8e0edfa53
Husain Hakim <husain.m.hakim.533@gmail.com>
2025-11-18 13:50:18 +0530

    refactor: cleaned admin routes imports and formatting

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,39 +2,47 @@ import { Router } from "express"
 import { authenticate } from "../middlewares/auth.js"
 import { requireAdmin } from "../middlewares/admin.js"
 import {
-	searchFilesInOrgAdmin,
 	listOrgsAdmin,
-	listUsersAdmin,
-	searchUsersInOrg,
 	createOrgAdmin,
-	addUserToOrgAdmin,
 	getOrgByIdAdmin,
 	updateOrgAdmin,
 	deleteOrgAdmin,
+	listUsersAdmin,
 	getUserByIdAdmin,
 	updateUserAdmin,
 	deleteUserAdmin,
+	searchUsersInOrg,
 	getUsersInOrgAdmin,
+	addUserToOrgAdmin,
 	removeUserFromOrgAdmin,
+	searchFilesInOrgAdmin,
 } from "../controllers/v1/admin.js"
 
 const router = Router()
 
+// All routes require authentication and admin privileges
 router.use(authenticate, requireAdmin)
 
+// Organization routes
 router.get("/orgs", listOrgsAdmin)
 router.post("/orgs", createOrgAdmin)
 router.get("/orgs/:orgId", getOrgByIdAdmin)
 router.patch("/orgs/:orgId", updateOrgAdmin)
 router.delete("/orgs/:orgId", deleteOrgAdmin)
+
+// User routes
 router.get("/users", listUsersAdmin)
 router.get("/users/search", searchUsersInOrg)
-router.post("/orgs/:orgId/users", addUserToOrgAdmin)
-router.get("/orgs/:orgId/users", getUsersInOrgAdmin)
 router.get("/users/:userId", getUserByIdAdmin)
 router.patch("/users/:userId", updateUserAdmin)
-router.delete("/orgs/:orgId/users/:userId", removeUserFromOrgAdmin)
 router.delete("/users/:userId", deleteUserAdmin)
+
+// Organization-User routes
+router.post("/orgs/:orgId/users", addUserToOrgAdmin)
+router.get("/orgs/:orgId/users", getUsersInOrgAdmin)
+router.delete("/orgs/:orgId/users/:userId", removeUserFromOrgAdmin)
+
+// File routes
 router.get("/orgs/:orgId/files/search", searchFilesInOrgAdmin)
 
 export default router
